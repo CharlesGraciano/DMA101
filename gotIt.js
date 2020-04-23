9762a8a32d63e7eed851a1c4d59fa8ccbfa82615
Pandora's Box <55153290+CharlesGraciano@users.noreply.github.com>
2020-04-22 17:10:34 -0700

    commit changes

---
+++ gotIt.js
@@ -0,0 +1,23 @@
+'use strict'
+
+function gotData (data){
+
+	fbData = data.val();
+
+	if (fbData){
+
+		console.log ('received data:');
+		console.log (fbData);
+		fbDataArray = Object.values(fbData);
+	} else {
+		console.log ('nothing in this folder yet');
+	}
+
+	}
+
+function errData (err){
+console.log ("error! didi not recieve data!");
+console.log(err);
+
+
+}
--- sketch.js
+++ sketch.js
@@ -1,7 +1,36 @@
+'use strict'
+
+
+let nodeData; //object we will puch to firbase 
+let fbData; // data we pull from firebase 
+let fbDataArray; // firibase data values converted to an array 
+let database; // refernece to our firebase database 
+let folderName = 'inbox'; //name of folder you create in db
+
+
 function setup() {
-  createCanvas(400, 400);
+ noCanvas();
+
+  let config = {
+    apiKey: "AIzaSyDJdRoS4rYe5hMwf8_LNWrEF0NLC3fKsS8",
+    authDomain: "text-me-dc031.firebaseapp.com",
+    databaseURL: "https://text-me-dc031.firebaseio.com",
+    projectId: "text-me-dc031",
+    storageBucket: "text-me-dc031.appspot.com",
+    messagingSenderId: "232930402370",
+    appId: "1:232930402370:web:0524cc10c396f9ebf70ddb",
+    
+  };
+
+  firebase.initializeApp(config);
+
+  database = firebase.database();
+
+  let ref= database.ref(folderName);
+
+  ref.on('value', gotData, errData);
 }
 
 function draw() {
-  background(220);
+  
 }
